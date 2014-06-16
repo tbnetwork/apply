4f425338f82c6c2056135f393098cff86410e7e0
Harrison Brown (iMac) <harrisonbro@gmail.com>
2014-06-16 09:59:44 +0100

    Re-builds project

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+/*global require, console, process */
+/*jshint strict:false */
+
+var css_src = 'src/css',
+    css_build = 'build/css',
+    js_src = 'src/js',
+    js_build = 'build/js';
+
+var gulp = require('gulp'),
+    notify = require('gulp-notify'),
+    autoprefix = require('gulp-autoprefixer'),
+    sass = require('gulp-ruby-sass'),
+    minifyCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglifyjs'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    spawn = require('child_process').spawn;
+
+gulp.task('livereload', function() {
+    var server = livereload();
+
+    gulp.watch([
+            'build/**',
+            'index.html'
+        ]).on('change', function(file) {
+            server.changed(file.path);
+        });
+});
+
+gulp.task('watch', function() {
+    var process;
+    function spawnChildren(e) {
+        if(process) { process.kill(); }
+        process = spawn('gulp', ['watching-task'], {stdio: 'inherit'});
+    }
+
+    gulp.watch('gulpfile.js', spawnChildren);
+    spawnChildren();
+});
+
+gulp.task('watching-task', ['livereload'], function(){
+    notify({
+        title: "Gulp is watching...",
+        message: "Don't forget to enable the LiveReload browser plugin."
+    });
+    gulp.watch(css_src + '/**/*.scss', ['css']);
+    gulp.watch(js_src + '/**/*.js', ['js']);
+});
+
+gulp.task('css', function() {
+    return gulp.src(css_src + '/styles.scss')
+        .pipe(sass({sourcemap: true, style: 'compact'}))
+        .pipe(autoprefix('last 10 version'))
+        .pipe(gulp.dest(css_build))
+        .pipe(notify("CSS built"));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+            js_src + '/jquery.min.js',
+            js_src + '/moment.js',
+            js_src + '/pikaday.js',
+            js_src + '/pikaday.jquery.js',
+            js_src + '/parsley.min.js',
+            js_src + '/jquery-ui-autocomplete.js',
+            js_src + '/jquery.select-to-autocomplete.min.js',
+            js_src + '/universities.js',
+            js_src + '/scripts.js'
+        ])
+        .pipe(uglify('main.js', {
+            outSourceMap: true,
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+                dead_code: true
+            }
+        }))
+        .pipe(gulp.dest(js_build))
+        .pipe(notify('Javascript built'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src([
+            css_build + '/**/*',
+            js_build + '/**/*'
+        ], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', ['clean', 'css', 'js']);
---
+++ src/js/scripts.js
@@ -0,0 +1,62 @@
+﻿jQuery(document).ready(function($){
+
+	/**
+	 * Date pickers
+	 */
+	var $dob = $('.dob').pikaday({
+		firstDay: 1,
+		defaultDate: new Date('1988-01-01'), 
+		minDate: new Date('1940-01-01'), 
+		maxDate: new Date('2999-12-31'), 
+		yearRange: [2000,2020],
+		format: 'DD/MM/YYYY'
+	});
+
+	var $availability = $('.availability').pikaday({
+		firstDay: 1,
+		minDate: new Date(), 
+		maxDate: new Date('2014-12-31'), 
+		yearRange: [2000,2020],
+		format: 'DD/MM/YYYY'
+	});
+
+	/**
+	 * Add another file button
+	 */
+	$('.add-file-field').click(function(){
+		// Find parent field wrapper
+		var parent = $(this).parent('.field'); 
+		// Get first hidden file input
+		var nextHidden = parent.find('.file.hidden').first(); 
+		// Show it
+		nextHidden.removeClass('hidden'); 
+		// Remove add button if there are no hidden items left
+		var filesLeftToAdd = parent.find('.file.hidden'); 
+		if( filesLeftToAdd.length == 0 ) { $(this).addClass('hidden'); }
+	}); 
+
+	/**
+	 * Disable submit button after submit
+	 */
+	$('form').submit(function(){
+		//$("input[type=sumit]", this).prop('disabled', true);
+		//return false; 
+	});
+
+	/**
+	 * Autocomplete country field
+	 */
+	$('.country-selector').selectToAutocomplete();
+	if ( !!( 'placeholder' in document.createElement('input') ) ) {
+    $( '.country-selector-hint' ).remove();
+  }
+
+  /**
+   * Autocomplete university field (UK unis only)
+   */
+  $('.university').autocomplete({
+  	source: universities, 
+  	delay: 0
+  }); 
+
+});
